c67c90c50c7b44c057d669a97a80b6c9ebf00ded
Edoardo Viganò <edo.vigano.29@gmail.com>
2023-01-02 22:41:00 +0100

    modify recipe detail

--- Project_Angular/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Project_Angular/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from "../recipes.model";
 
 @Component({
@@ -8,16 +8,24 @@ import {Recipe} from "../recipes.model";
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe',
       'This is simply a test',
       'https://dcassetcdn.com/design_img/10150/25224/25224_294121_10150_image.jpg'),
+    new Recipe('Another Test Recipe',
+      'This is simply a test',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272')
 
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe : Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
